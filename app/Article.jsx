9e69dfbfd69c05e5ae7effa4863961890453fd94
lenny <lenmorld@gmail.com>
2018-08-29 23:06:14 -0400

    render server-generated html-from-markdown into React component

--- app/Article.jsx
+++ app/Article.jsx
@@ -3,14 +3,52 @@ import ReactDOM from 'react-dom';
 import Markdown from 'react-markdown';
 
 class Article extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      html: ''
+    }
+  }
+
+  componentDidMount() {
+    // response we get from fetch() is not JSON
+    // https://scotch.io/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
+
+    fetch("api/article").then(res =>
+      res.text()
+      // use res.json() if expecting JSON
+    ).then(data => {
+      console.log(data);
+
+      this.setState({
+        html: data
+      })
+    })
+  }
+
+  createMarkup(html) {
+    // sanitize
+    const div = document.createElement("div");
+    div.innerHTML = html;
+    div.querySelectorAll('script').forEach(e => e.parentNode.removeChild(e));
+    div.querySelectorAll('iframe').forEach(e => e.parentNode.removeChild(e));
+
+    return div.innerHTML;
+  }
+
   render() {
     debugger;
     return (
       <div className="card">
-        <a href="api/article">Article_01</a>
+        <a href="api/article">full screen</a>
+        <div dangerouslySetInnerHTML={{__html: this.createMarkup(this.state.html)}}>
+        </div>
 
         {/*<Markdown source={this.props.article.body} />*/}
       </div>
+
     );
   }
 };
